0af24a0445919a93f1b33cda21bb13ff5859a3db
aispark <aispark@paran.com>
2019-12-10 15:29:47 +0900

    cors 설정 변경

--- src/app.js
+++ src/app.js
@@ -14,7 +14,16 @@ import {
 import { alpsUploadOrder, alpsExportInvoice } from "@/mainComponents/alps";
 import { importOrder, importInvoice } from "@/mainComponents/excelHandler";
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 // app.all("/*", function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
