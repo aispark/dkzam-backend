c136cb8b0d53df25d995a08ce51746dd2db5a9ef
aispark <aispark@paran.com>
2019-12-11 15:31:20 +0900

    불필요한 상수값 제거

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,4 @@
 const EXPORT_ORDER_PATH = `${__dirname}/order.xlsx`.replace(/\\/g, "/");
 const EXPORT_INVOICE_PATH = `${__dirname}/invoice.xlsx`.replace(/\\/g, "/");
-const EXPORT_DISPLAY_INVOICE_PATH = `${__dirname}/display_invoice.xlsx`.replace(
-  /\\/g,
-  "/"
-);
 
-export { EXPORT_ORDER_PATH, EXPORT_INVOICE_PATH, EXPORT_DISPLAY_INVOICE_PATH };
+export { EXPORT_ORDER_PATH, EXPORT_INVOICE_PATH };
