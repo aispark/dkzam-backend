f4f941f7311e55bed81f76b261ba628a87e6850b
aispark <aispark@paran.com>
2019-12-11 15:32:36 +0900

    송장등록 파일 생성 function 추가

--- src/mainComponents/excelHandler.js
+++ src/mainComponents/excelHandler.js
@@ -1,7 +1,7 @@
 import { EXPORT_ORDER_PATH, EXPORT_INVOICE_PATH } from "@/constants";
-const XLSX = require("xlsx");
+import XLSX from "xlsx";
 
-//스마트 스토어 발주완료 목록을 excel 파일로 export한다.
+//스마트 스토어 발주완료 목록을 import 한다.
 async function importOrder() {
   try {
     const workbook = XLSX.readFile(EXPORT_ORDER_PATH);
@@ -27,7 +27,39 @@ async function importOrder() {
   }
 }
 
-//스마트 스토어 발주완료 목록을 excel 파일로 export한다.
+async function exportSmartStoreInvoice(dest, invoiceNoList) {
+  let { resultList } = await importInvoice();
+
+  resultList = resultList.filter(item =>
+    invoiceNoList.some(invoiceNo => invoiceNo === item.invoiceNo)
+  );
+
+  let invoiceList = resultList.map(item => {
+    const data = [
+      String(item.orderNo),
+      "택배,등기,소포",
+      "롯데택배",
+      String(item.invoiceNo)
+    ];
+    return data;
+  });
+
+  invoiceList = [
+    ["상품주문번호", "배송방법", "택배사", "송장번호"],
+    ...invoiceList
+  ];
+
+  const ws = XLSX.utils.aoa_to_sheet(invoiceList);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, "발송처리");
+
+  XLSX.writeFile(wb, dest, {
+    bookType: "xlsx",
+    bookSST: "false"
+  });
+}
+
+//롯데택배 송장 목록을 import 한다.
 async function importInvoice() {
   try {
     const workbook = XLSX.readFile(EXPORT_INVOICE_PATH);
@@ -39,8 +71,9 @@ async function importInvoice() {
         const data = {
           no: index + 1,
           orderNo: item.상품주문번호,
-          deliveryMethod: item.배송방법,
-          deliveryCompany: item.택배사,
+          acperNm: item.수취인명,
+          acperTel: item.전화번호,
+          gdsNm: item.상품명,
           invoiceNo: item.송장번호
         };
         return data;
@@ -59,4 +92,4 @@ async function importInvoice() {
   }
 }
 
-export { importOrder, importInvoice };
+export { importOrder, importInvoice, exportSmartStoreInvoice };
