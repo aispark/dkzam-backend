41fafb024aa5e3906c858da3c441435e66c31371
aispark <aispark@paran.com>
2019-12-11 15:31:40 +0900

    소스 리팩토링

--- src/mainComponents/alps.js
+++ src/mainComponents/alps.js
@@ -1,33 +1,27 @@
 import { EXPORT_ORDER_PATH, EXPORT_INVOICE_PATH } from "@/constants";
 import app from "@/app";
-const puppeteer = require("puppeteer");
-const XLSX = require("xlsx");
-const Stopwatch = require("statman-stopwatch");
+import puppeteer from "puppeteer";
+import XLSX from "xlsx";
+import Stopwatch from "statman-stopwatch";
 import { importInvoice } from "@/mainComponents/excelHandler";
-
-let executablePath =
-  "node_modules/puppeteer/.local-chromium/win64-706915/chrome-win/chrome.exe";
-
-if (process.env.NODE_ENV === "production")
-  executablePath = `${__dirname}/${executablePath}`;
+import {
+  alpsLogin,
+  waitForClick,
+  launchPuppeteer
+} from "@/mainComponents/commonPuppeteer";
 
 //alps 송장 export
-async function alpsExportInvoice() {
-  const logChannel = "process:log";
-  const sendLog = (channel, message) => app.io.emit(channel, message);
+async function alpsExportInvoice(socketId) {
+  const logChannel = "invoiceProcess:log";
+  const sendLog = (channel, message) =>
+    app.io.to(`${socketId}`).emit(channel, message);
 
   const sw = new Stopwatch(true);
 
   sendLog(logChannel, `송장 가져오기 시작 ${sw.read(0) / 1000} seconds`);
 
-  const browser = await puppeteer.launch({
-    // headless: false,
-    executablePath,
-    slowMo: 20,
-    args: [
-      "--start-maximized" // you can also use '--start-fullscreen'
-    ]
-  });
+  const browser = await launchPuppeteer();
+
   try {
     const page = await browser.newPage();
 
@@ -42,61 +36,34 @@ async function alpsExportInvoice() {
     await navigationPromise;
     sendLog(logChannel, `로그인 시작 ${sw.read(0) / 1000} seconds`);
 
-    await page.waitForSelector(".login_form > ul > .ID > #principal > .input");
-    await page.click(".login_form > ul > .ID > #principal > .input");
-    await page.type(
-      ".login_form > ul > .ID > #principal > .input",
-      process.env.VUE_APP_ALPS_ID
-    );
-
-    await page.waitForSelector(
-      ".login_form > ul > .Password > #credential > .input"
-    );
-    await page.click(".login_form > ul > .Password > #credential > .input");
-    await page.type(
-      ".login_form > ul > .Password > #credential > .input",
-      process.env.VUE_APP_ALPS_PASSWORD
-    );
-
-    await page.waitForSelector(".login_box #btn-login");
-    await page.click(".login_box #btn-login");
+    await alpsLogin(page);
 
     sendLog(logChannel, `로그인 완료 ${sw.read(0) / 1000} seconds`);
 
     await navigationPromise;
 
     await page.waitFor(2000);
-    await page.waitForSelector("header > nav > .row [text='집배달']");
-    await page.click("header > nav > .row [text='집배달']");
 
-    await page.waitFor(2000);
+    await waitForClick(page, "header > nav > .row [text='집배달']");
 
     sendLog(logChannel, `대메뉴 집배달 클릭 ${sw.read(0) / 1000} seconds`);
+    await page.waitFor(2000);
 
-    await page.waitForSelector(
+    await waitForClick(
+      page,
       ".subMenuContent > .row:nth-child(4) > .col-3:nth-child(1) > ul > li:nth-child(1) > .menuLeaf"
     );
-    await page.click(
-      ".subMenuContent > .row:nth-child(4) > .col-3:nth-child(1) > ul > li:nth-child(1) > .menuLeaf"
-    );
-
-    await page.waitFor(5000);
 
     sendLog(logChannel, `통합 운송장 관리 클릭 ${sw.read(0) / 1000} seconds`);
 
+    await page.waitFor(5000);
+
     let frames = await page.frames();
     const frame_904 = frames.find(f => {
-      // console.log("f.url()", f.url());
       return f.url().indexOf("/pid/pages/pic") > 0;
     });
 
-    // const childFrames = await frame_904.content();
-    // console.log("childFrames", childFrames);
-
-    // console.log("frame_904", frame_904);
-
-    await frame_904.waitForSelector(".searchBtnGroup [text='조회']");
-    await frame_904.click(".searchBtnGroup [text='조회']");
+    await waitForClick(frame_904, ".searchBtnGroup [text='조회']");
 
     const finalResponse = await page.waitForResponse(
       response => response.url().indexOf("/pid/pic/intgmgrinvprnts") > 0
@@ -104,24 +71,33 @@ async function alpsExportInvoice() {
 
     const { dsRsrvList } = await finalResponse.json();
 
-    // console.log("dsRsrvList", dsRsrvList);
-
     //더쿠잼 인 데이터만 export 한다.
     const filterList = dsRsrvList.filter(
       item => item.snperNm === "더쿠잼" && item.invPrntYn === "Y"
     );
 
-    const sendList = filterList.map(item => {
-      const data = [item.ordNo, "택배,등기,소포", "롯데택배", item.invNo];
+    // const sendList = filterList.map(item => {
+    //   const data = [item.ordNo, "택배,등기,소포", "롯데택배", item.invNo];
+    //   return data;
+    // });
+
+    let invoiceList = filterList.map(item => {
+      const data = [
+        String(item.ordNo),
+        item.acperNm,
+        item.acperTel,
+        item.gdsNm,
+        String(item.invNo)
+      ];
       return data;
     });
 
-    const exportList = [
-      ["상품주문번호", "배송방법", "택배사", "송장번호"],
-      ...sendList
+    invoiceList = [
+      ["상품주문번호", "수취인명", "전화번호", "상품명", "송장번호"],
+      ...invoiceList
     ];
 
-    const ws = XLSX.utils.aoa_to_sheet(exportList);
+    const ws = XLSX.utils.aoa_to_sheet(invoiceList);
 
     /* add to workbook */
     const wb = XLSX.utils.book_new();
@@ -153,20 +129,18 @@ async function alpsExportInvoice() {
   }
 }
 
-async function alpsUploadOrder() {
-  const logChannel = "process:log";
-  const sendLog = (channel, message) => app.io.emit(channel, message);
+//발주확인 완료 등록
+async function alpsUploadOrder(socketId) {
+  const logChannel = "orderProcess:log";
+  const sendLog = (channel, message) =>
+    app.io.to(`${socketId}`).emit(channel, message);
 
   const sw = new Stopwatch(true);
 
-  const browser = await puppeteer.launch({
-    // headless: false,
-    executablePath,
-    slowMo: 20,
-    args: [
-      "--start-maximized" // you can also use '--start-fullscreen'
-    ]
-  });
+  sendLog(logChannel, `발주확인 완료 등록 시작 ${sw.read(0) / 1000} seconds`);
+
+  const browser = await launchPuppeteer();
+
   try {
     const page = await browser.newPage();
 
@@ -178,41 +152,25 @@ async function alpsUploadOrder() {
 
     await page.setViewport({ width: 1920, height: 1080 });
 
-    await page.waitForSelector(".login_form > ul > .ID > #principal > .input");
-    await page.click(".login_form > ul > .ID > #principal > .input");
-    await page.type(
-      ".login_form > ul > .ID > #principal > .input",
-      process.env.VUE_APP_ALPS_ID
-    );
+    await navigationPromise;
+    sendLog(logChannel, `로그인 시작 ${sw.read(0) / 1000} seconds`);
 
-    await page.waitForSelector(
-      ".login_form > ul > .Password > #credential > .input"
-    );
-    await page.click(".login_form > ul > .Password > #credential > .input");
-    await page.type(
-      ".login_form > ul > .Password > #credential > .input",
-      process.env.VUE_APP_ALPS_PASSWORD
-    );
-
-    await page.waitForSelector(".login_box #btn-login");
-    await page.click(".login_box #btn-login");
+    await alpsLogin(page);
 
     sendLog(logChannel, `로그인 완료 ${sw.read(0) / 1000} seconds`);
 
     await navigationPromise;
 
     await page.waitFor(2000);
-    await page.waitForSelector("header > nav > .row [text='거래처관리']");
-    await page.click("header > nav > .row [text='거래처관리']");
+
+    await waitForClick(page, "header > nav > .row [text='거래처관리']");
 
     await page.waitFor(2000);
 
     sendLog(logChannel, `대메뉴 거래처관리 클릭 ${sw.read(0) / 1000} seconds`);
 
-    await page.waitForSelector(
-      ".subMenuContent > .row:nth-child(2) > .col-3:nth-child(2) > ul > li:nth-child(2) > .menuLeaf"
-    );
-    await page.click(
+    await waitForClick(
+      page,
       ".subMenuContent > .row:nth-child(2) > .col-3:nth-child(2) > ul > li:nth-child(2) > .menuLeaf"
     );
 
@@ -227,8 +185,7 @@ async function alpsUploadOrder() {
     });
 
     //사용자 파일 설정
-    await frame_2032.waitForSelector("#cboUsrFmat01");
-    await frame_2032.click("#cboUsrFmat01");
+    await waitForClick(frame_2032, "#cboUsrFmat01");
     await page.waitFor(500);
     await page.keyboard.press("ArrowDown");
     await page.waitFor(500);
@@ -236,12 +193,10 @@ async function alpsUploadOrder() {
 
     //타이틀 있음 체크
     await page.waitFor(2000);
-    await frame_2032.waitForSelector("#chkTitleYn01 > .label");
-    await frame_2032.click("#chkTitleYn01 > .label");
+    await waitForClick(frame_2032, "#chkTitleYn01 > .label");
 
     //합포장처리
-    await frame_2032.waitForSelector("#chkBdpkBaseCd01 > .button");
-    await frame_2032.click("#chkBdpkBaseCd01 > .button");
+    await waitForClick(frame_2032, "#chkBdpkBaseCd01 > .button");
 
     //파일 업로드
     const fileInput = await frame_2032.$("input[type=file]");
