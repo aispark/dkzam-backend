32767f894aaa0b8e10a9763dbbf6b12c1edea795
aispark <aispark@paran.com>
2019-12-11 15:31:05 +0900

    req get url 추가

--- src/app.js
+++ src/app.js
@@ -37,42 +37,60 @@ app.get("/", async (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
-//발주완료 목록
-app.get("/smartStore/orderList", async (req, res) => {
-  const result = await exportOrder(req.query.socketId);
-  res.send(result);
-});
-
 //발주확인 처리
 app.get("/smartStore/placeOrder", async (req, res) => {
   const result = await placeOrder(req.query.socketId);
   res.send(result);
 });
 
-// connection event handler
-// connection이 수립되면 event handler function의 인자로 socket인 들어온다
-app.io.on("connection", async function(socket) {
-  console.log("socket.id", socket.id);
-  // 접속한 클라이언트의 정보가 수신되면
-  // socket.on("processMessage", function(data) {
-  //   console.log(
-  //     "Client logged-in:\n name:" + data.name + "\n userid: " + data.userId
-  //   );
-
-  //   // socket에 클라이언트 정보를 저장한다
-  //   socket.name = data.name;
-  //   socket.userid = data.userid;
-
-  //   // 접속된 클라이언트에게 메시지를 전송한다
-  //   app.io.emit("processMessage", data.name);
-  // });
-
-  // console.log(app.io.sockets);
-
-  socket.on("disconnect", function() {
-    console.log("user disconnected: " + socket.id);
-  });
+//발주완료 목록
+app.get("/smartStore/orderList", async (req, res) => {
+  const result = await exportOrder(req.query.socketId);
+  res.send(result);
 });
+
+//발주완료 목록 등록
+app.get("/alps/uploadOrder", async (req, res) => {
+  const result = await alpsUploadOrder(req.query.socketId);
+  res.send(result);
+});
+
+//송장 목록
+app.get("/alps/invoiceList", async (req, res) => {
+  const result = await alpsExportInvoice(req.query.socketId);
+  res.send(result);
+});
+
+//송장 목록 등록
+app.get("/smartStore/uploadInvoice", async (req, res) => {
+  const result = await uploadInvoice(req.query);
+  res.send(result);
+});
+
+//송장 파일 목록
+app.get("/alps/importInvoiceList", async (req, res) => {
+  const result = await importInvoice();
+  res.send(result);
+});
+
+// connection이 수립되면 event handler function의 인자로 socket인 들어온다
+// app.io.on("connection", async function(socket) {
+// 접속한 클라이언트의 정보가 수신되면
+// socket.on("processMessage", function(data) {
+//   console.log(
+//     "Client logged-in:\n name:" + data.name + "\n userid: " + data.userId
+//   );
+//   // socket에 클라이언트 정보를 저장한다
+//   socket.name = data.name;
+//   socket.userid = data.userid;
+//   // 접속된 클라이언트에게 메시지를 전송한다
+//   app.io.emit("processMessage", data.name);
+// });
+
+//   socket.on("disconnect", function() {
+//     console.log("user disconnected: " + socket.id);
+//   });
+// });
 
 server.listen(process.env.PORT || 3000, function() {
   console.log(`Socket IO server listening on port ${process.env.PORT || 3000}`);
